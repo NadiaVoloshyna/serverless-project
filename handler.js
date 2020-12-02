a15c5a75dd38939481dcb698a8985340e798f5df
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-12-02 13:10:53 +0200

    added post

--- handler.js
+++ handler.js
@@ -20,3 +20,38 @@ module.exports.hello = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.goodbye = (event, context, callback) => {
+  let message = 'HELLO WORLD!';
+  const name = event.pathParameters && event.pathParameters.name;
+  if(name !== null) {
+    message = 'Hello ' + name;
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: message,
+        input: event,
+      }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.hi = (event, context, callback) => {
+  
+  const body = JSON.parse(event.body);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: body,
+        input: event,
+      }),
+  };
+
+  callback(null, response);
+};
